79512cb6791542a1f9b8c3121d630c3bbbde4201
varungore <varun@fab.com>
2014-05-09 15:08:48 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbLoginTest::Application.config.secret_token = '3b26afc4bf40359a17fd987af0943561f880207b245cab0117ed2123d84fd93986787c799a95ce0d95c2e0a09351c51bba8b06402f97cc9db651818d334279c4'
